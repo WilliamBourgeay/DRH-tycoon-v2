e7dabf40e1bfd0ee5374ab336dfc62687cb192c1
unknown <sebastien.roynette@gmail.com>
2018-12-13 21:52:19 +0100

    DRH Tycoon first commit

---
+++ DRH Tycoon/Assets/Employee_Example.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Employee_Example : MonoBehaviour {
+
+    public string astro = "Cancer";
+    public int sex = 0;
+    public int age = 32;
+    public string blood = "O+";
+    public string hair_color = "Brun";
+    public string eye_color = "Bleu";
+    public string skin_color = "Blanc";
+
+    public double social = 0.30;
+    public double experience = 0.30;
+    public double motivation = 0.30;
+    public int salary = 45000;
+
+    public bool corporate = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
